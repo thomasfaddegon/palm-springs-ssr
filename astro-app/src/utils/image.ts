caf96d1c4777b2e6b9e88dc9dc942a52a4919274
Thomas Faddegon <tomfaddegon@gmail.com>
2026-04-10 13:37:46 -0400

    updated sanity client

--- astro-app/src/utils/image.ts
+++ astro-app/src/utils/image.ts
@@ -1,6 +1,17 @@
 import imageUrlBuilder from "@sanity/image-url";
 import type { Image } from "@sanity/types";
-import { sanityClient } from "sanity:client";
+import { createClient } from "@sanity/client";
+
+const sanityClient = createClient({
+  projectId:
+    import.meta.env.PUBLIC_SANITY_STUDIO_PROJECT_ID ||
+    import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset:
+    import.meta.env.PUBLIC_SANITY_STUDIO_DATASET ||
+    import.meta.env.PUBLIC_SANITY_DATASET,
+  apiVersion: "2024-12-08",
+  useCdn: true,
+});
 
 const builder = imageUrlBuilder(sanityClient);
 
